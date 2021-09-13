1a191d85bd2fdeca58e4c17b590cc588cf1efb03
tovd-go <994425414@qq.com>
2021-09-13 23:31:06 +0800

    第一次上传

--- src/main/java/master/tmexp/Menu_top.java
+++ src/main/java/master/tmexp/Menu_top.java
@@ -13,54 +13,51 @@ import java.util.Optional;
 
 public class Menu_top {
     public static void mianzhe(){
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("免责声明！");
-        alert.setContentText("该工具只供学习使用，任何组织，个人利用该工具从事非法工作均与开发者无关！");
-        alert.setHeaderText(null);
-        alert.showAndWait();
-//
-//        int OK= JOptionPane.showConfirmDialog(null, "该工具只供学习使用，任何组织，个人利用该工具从事非法工作均与开发者无关！",
-//                "免责声明",JOptionPane.YES_NO_OPTION);
-//        if (OK==1){
-//            System.exit(0);
-//        }
+//        Alert alert = new Alert(Alert.AlertType.WARNING);
+//        alert.setTitle("免责声明！");
+//        alert.setContentText("该工具只供学习使用，任何组织，个人利用该工具从事非法工作均与开发者无关！");
+//        alert.setHeaderText(null);
+//        alert.showAndWait();
+
+        int OK= JOptionPane.showConfirmDialog(null, "该工具只供学习使用，任何组织，个人利用该工具从事非法工作均与开发者无关！",
+                "免责声明",JOptionPane.YES_NO_OPTION);
+        if (OK==1){
+            System.exit(0);
+        }
 
     }
     public Menu shezhi(){
         Menu menu =new Menu("设置");
         MenuItem menuItem=new MenuItem("代理");
-        menuItem.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                Stage stage =new Stage();
-                GridPane gridPane =new GridPane();
-                CheckBox checkBox =new CheckBox();
-                if (Value_static.checkBox_proxy){
-                    checkBox.setSelected(true);
-                }
-                Label label_host =new Label("HOST:");
-                Label label_port =new Label("PORT:");
-                Label label_title=new Label();
-                TextField textField_host=new TextField();
-                TextField textField_port =new TextField();
-                textField_host.setText(Value_static.proxy_host);
-                textField_port.setText(String.valueOf(Value_static.proxy_port));
-                textField_port.setPrefWidth(60);
-                gridPane.setPadding(new Insets(10,10,10,10));
-                gridPane.setHgap(5);
-                gridPane.add(label_host,1,0);
-                gridPane.add(textField_host,2,0);
-                gridPane.add(label_port,3,0);
-                gridPane.add(textField_port,4,0);
-                gridPane.add(label_title,2,1);
-                checkBox=Action_Listen.checkBox_Proxy_listen(checkBox,textField_host,textField_port,label_title);
-                gridPane.add(checkBox,5,0);
-                Scene scene =new Scene(gridPane,400,200);
-                stage.setScene(scene);
-                stage.initModality(Modality.APPLICATION_MODAL);
-                stage.show();
-
+        menuItem.setOnAction(actionEvent -> {
+            Stage stage =new Stage();
+            GridPane gridPane =new GridPane();
+            CheckBox checkBox =new CheckBox();
+            if (Value_static.checkBox_proxy){
+                checkBox.setSelected(true);
             }
+            Label label_host =new Label("HOST:");
+            Label label_port =new Label("PORT:");
+            Label label_title=new Label();
+            TextField textField_host=new TextField();
+            TextField textField_port =new TextField();
+            textField_host.setText(Value_static.proxy_host);
+            textField_port.setText(String.valueOf(Value_static.proxy_port));
+            textField_port.setPrefWidth(60);
+            gridPane.setPadding(new Insets(10,10,10,10));
+            gridPane.setHgap(5);
+            gridPane.add(label_host,1,0);
+            gridPane.add(textField_host,2,0);
+            gridPane.add(label_port,3,0);
+            gridPane.add(textField_port,4,0);
+            gridPane.add(label_title,2,1);
+            checkBox=Action_Listen.checkBox_Proxy_listen(checkBox,textField_host,textField_port,label_title);
+            gridPane.add(checkBox,5,0);
+            Scene scene =new Scene(gridPane,400,200);
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+
         });
         menu.getItems().add(menuItem);
         return menu;
